d781c48e6e7ec21ab29c96e1d51b67d19c908c8b
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-04-10 21:42:27 +0300

    Fix tests

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -10,62 +10,66 @@
                      (results-status status)))
                  '(stuff))))
 
+(defun gen-descriptors (n m)
+  "Make n descriptors with m independent variables."
+  (lambda ()
+    (let* ((n (if (functionp n) (funcall n) n))
+           (m (if (functionp m) (funcall m) m))
+           (result (make-array (list n util:+descriptor-length+)
+                               :element-type 'single-float
+                               :initial-element 0.0)))
+      (loop for i below n do
+        (loop for j below m do
+          (setf (aref result i j) (random 1f0))))
+      result)))
+
+(defun gen-coord ()
+  (lambda ()
+    (make-array 3 :element-type 'single-float
+                  :initial-contents
+                  (loop repeat 3 collect (random 100.0)))))
+
+(defun gen-pair (gen)
+  (lambda ()
+    (cons (funcall gen)
+          (funcall gen))))
+
 (in-suite stuff)
 
 (test pca
-  (loop repeat 100
-        ;; Number of samples
-        for samples = (+ 1000 (random 1000))
-        ;; Number of independent variables
-        for indep   = (+ 50 (random 100))
-        for descr   = (make-array (list samples util:+descriptor-length+)
-                                  :element-type 'single-float
-                                  :initial-element 0.0)
-        do
-           (loop for i below samples do
-             (loop for j below indep do
-               (setf (aref descr i j) (random 1.0))))
-           (multiple-value-bind (trans means)
-               (pca:fit-pca descr 0.99)
-             (is (<= (array-dimension trans 0)
-                     (1+ indep)))
-             (let* ((descr-pcad (pca:transform-pca descr trans means))
-                    (descr-back (pca:invert-pca descr-pcad trans means)))
-               (is-true (array-approx-p descr descr-back :rtol 1f-1))))))
+  (for-all* ((samples (gen-integer :min 1000 :max 2000))
+             (indep   (gen-integer :min 50   :max 150))
+             (descr   (gen-descriptors samples indep)))
+    (multiple-value-bind (trans means)
+        (pca:fit-pca descr 0.99)
+      (is (<= (array-dimension trans 0)
+              (1+ indep)))
+      (let* ((descr-pcad (pca:transform-pca descr trans means))
+             (descr-back (pca:invert-pca descr-pcad trans means)))
+        (is-true (array-approx-p descr descr-back :rtol 1f-1))))))
 
 (test ransac
-  (flet ((random-vec ()
-           (make-array 3
-                       :element-type 'single-float
-                       :initial-contents
-                       (loop repeat 3 collect (random 100.0)))))
-    (loop repeat 50
-          ;; Number of inliers
-          for inliers = (+ 2000 (random 1000))
-          ;; Number of outliers
-          for outliers = (+ 200 (random 100))
-          ;; A simple transform
-          for translation = (random-vec)
-          for matches-good = (loop repeat inliers
-                                   for p1 = (random-vec)
-                                   for p2 = (map '(vector single-float) #'+
-                                                 p1 translation)
-                                   collect (cons p1 p2))
-          for matches-bad = (loop repeat outliers
-                                  collect (cons (random-vec)
-                                                (random-vec)))
-          for matches = (append matches-good matches-bad)
-          for transform = (tran:ransac #'tran:rigid-transform-fit matches
-                                       :max-iter 1000 :err 10.0)
-          do
-             (loop for i below 3 do
-               (is-true
-                (approxp (aref transform i 3)
-                         (aref translation i)
-                         :rtol 1f-2)))
-             (loop for i below 3 do
-               (loop for j below 3 do
-                 (let ((x (aref transform i j)))
-                   (if (= i j)
-                       (is (approxp x 1.0))
-                       (< (abs x) 1f-5))))))))
+  (for-all ((inliers  (gen-list :length   (gen-integer :min 2000 :max 3000)
+                                :elements (gen-coord)))
+            (outliers (gen-list :length   (gen-integer :min 500 :max 1500)
+                                :elements (gen-pair (gen-coord))))
+            (translation (gen-coord)))
+    (let* ((inliers (mapcar
+                     (lambda (v)
+                       (cons v (map '(vector single-float) #'+
+                                    v translation)))
+                     inliers))
+           (all-together (append inliers outliers))
+           (transform (tran:ransac (tran:rigid-transform-fit) all-together
+                                   :iterations 400 :err 2.0)))
+      (loop for i below 3 do
+        (is-true
+         (approxp (aref transform i 3)
+                  (aref translation i)
+                  :rtol 1f-2)))
+      (loop for i below 3 do
+        (loop for j below 3 do
+          (let ((x (aref transform i j)))
+            (if (= i j)
+                (is (approxp x 1.0))
+                (< (abs x) 1f-5))))))))
